951b4d1d3cff50e184ed1160b3736fa3fb19b4fc
Anthony Bisulco <bisulco.a@husky.neu.edu>
2018-01-08 21:15:29 -0500

    Added in gpu temp function + constant colors

--- lights.py
+++ lights.py
@@ -1,8 +1,16 @@
+"""
+Code by Anthony Bisulco to control Blinky Tape lights
+"""
 import platform
 import sys
 import logging
+import re
+import math
+from time import sleep
 from subprocess import Popen, PIPE
 from BlinkyTape import BlinkyTape
+from color_constants import RGB
+from collections import namedtuple, OrderedDict
 
 
 def find_usb_dev():
@@ -32,6 +40,36 @@ def find_usb_dev():
         return -1
 
 
+def set_static_color(col, bt):
+    """[sets a static color on the led strip]
+
+    Arguments:
+        col {[type]} -- [color from RGB class]
+        bt {[BlinkyTape]} -- [light controller object]
+    """
+    bt.displayColor(col.red, col.green, col.blue)
+    logging.debug("Set Color to (%s,%s,%s)" % (col.red, col.green, col.blue))
+
+
+def gpu_color(bt):
+    """[function to light strip based on gpu temperature]
+
+    Arguments:
+        bt {[BlinkyTape]} -- [light controller object]
+    """
+    if platform.system() == "Linux":
+        process = Popen("nvidia-smi",
+                        shell=True, stdout=PIPE, stderr=PIPE)
+    elif platform.system() == "Windows":
+        logging.debug("Working on Windows Support")
+    std_out, std_err = process.communicate()
+    gpu_temp = int(re.search("\d\dC",
+                             std_out.decode("utf8")).group().split("C")[0])
+    RGB = namedtuple('RGB', 'red, green, blue')
+    set_static_color(RGB(math.floor(1.04 * gpu_temp),
+                         math.floor(100 - 1.04 * gpu_temp), 0), bt)
+
+
 def main():
     """[Main function to run custom light program]
     """
@@ -40,6 +78,7 @@ def main():
     logging.getLogger().addHandler(logging.StreamHandler())
     logging.debug('Begining to Run Program')
     usb_devices = find_usb_dev()
+    bt = BlinkyTape('/dev/' + usb_devices.split('\n')[0])
 
 
 if __name__ == '__main__':
